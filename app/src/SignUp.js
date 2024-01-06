0db32ad1c199f378cbe2903ac0882e149e78913b
ankit-suman-07 <ankitsuman07@gmail.com>
2024-01-07 03:08:34 +0530

    css: page design implemented

--- app/src/SignUp.js
+++ app/src/SignUp.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import "./style.css";
+
+import Logo from "./assets/logo.jpg";
 
 const SignUp = () => {
     const [signUpData, setSignUpData] = useState({
@@ -9,15 +12,14 @@ const SignUp = () => {
     });
 
     const [message, setMessage] = useState("");
+    const [greet, setGreet] = useState("");
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:8000/register', signUpData);
-            console.log(response.data.message);
-            setMessage(response.data.message);
+            setGreet(response.data.message);
         } catch (error) {
-            console.log(error.response.data.error);
             setMessage(error.response.data.error);
         }
         setSignUpData({
@@ -34,41 +36,50 @@ const SignUp = () => {
         }));
     }
     return (
-        <div>
-            <h3>SignUp Page</h3>
-            <form onSubmit={handleFormSubmit} >
-                <input
-                    type='text'
-                    name='username'
-                    placeholder='Username'
-                    value={signUpData.username}
-                    onChange={handleSignUpChange}
-                    required
-                />
-                <input
-                    type='password'
-                    name='password'
-                    placeholder='Password'
-                    value={signUpData.password}
-                    onChange={handleSignUpChange}
-                    required
-                />
-                <p>
-                    {
-                        <span>{message}</span>
-                    }
-                </p>
-                <button type='submit' >
-                    Sign Up
-                </button>
+        <div className='outer' >
+            <div className='box' >
+                <div className='image' >
+                    <img src={Logo} />
+                </div>
+                <div className='form' >
+                    <div className='head' >
+                        Sign Up
+                    </div>
+                    <div className='inner' >
+                        <span className='error' >{message}</span>
+                        <span className='greet' >{greet}</span>
+                        <form onSubmit={handleFormSubmit} >
+                            <input
+                                type='text'
+                                name='username'
+                                placeholder='Username'
+                                value={signUpData.username}
+                                onChange={handleSignUpChange}
+                                required
+                            />
+                            <input
+                                type='password'
+                                name='password'
+                                placeholder='Password'
+                                value={signUpData.password}
+                                onChange={handleSignUpChange}
+                                required
+                            />
+                            <button type='submit' >
+                                Sign Up
+                            </button>
+                        </form>
 
-                <p>
-                    Already registered?
-                    <Link to='/login' >
-                        Login
-                    </Link>
-                </p>
-            </form>
+                    </div>
+                    <div className='bottom' >
+                        Already registered?
+                        <Link to='/login' >
+                            Login
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
